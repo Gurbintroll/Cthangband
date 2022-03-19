aea56fc3d8ad430e483c5b3696aaff79d9529465
Cthangband <99888526+Cthangband@users.noreply.github.com>
2022-03-19 17:37:26 +0000

    Up the version number to 6.9

--- Cthangband/Properties/AssemblyInfo.cs
+++ Cthangband/Properties/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿using System.Reflection;
+﻿// Cthangband: © 1997 - 2022 Dean Anderson; Based on Angband: © 1997 Ben Harrison, James E. Wilson,
+// Robert A. Koeneke; Based on Moria: © 1985 Robert Alan Koeneke and Umoria: © 1989 James E.Wilson
+//
+// This game is released under the “Angband License”, defined as: “© 1997 Ben Harrison, James E.
+// Wilson, Robert A. Koeneke This software may be copied and distributed for educational, research,
+// and not for profit purposes provided that this copyright and statement are included in all such
+// copies. Other copyrights may also apply.”
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following set of attributes.
@@ -25,4 +32,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.8.*")]
+[assembly: AssemblyVersion("6.9.*")]
