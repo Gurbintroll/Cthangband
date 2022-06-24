8a56be1e00cd8b4470c73bc5160bfa4c6027baa6
Cthangband <99888526+Cthangband@users.noreply.github.com>
2022-06-24 13:00:21 +0100

    Code Refactoring

--- Cthangband/ExPlayer.cs
+++ Cthangband/ExPlayer.cs
@@ -10,19 +10,62 @@ using System;
 
 namespace Cthangband
 {
+    /// <summary>
+    /// A dead player character, holding just the bare bones needed for the high score table and
+    /// savegame preview, or to create a new character based on the previous one.
+    /// </summary>
     [Serializable]
     internal class ExPlayer
     {
+        /// <summary>
+        /// The index of the character's gender
+        /// </summary>
         public readonly int GenderIndex;
+
+        /// <summary>
+        /// The character's generation, to be appended to their name
+        /// </summary>
         public readonly int Generation;
+
+        /// <summary>
+        /// The level at which the character died
+        /// </summary>
         public readonly int Level;
+
+        /// <summary>
+        /// The name of the character
+        /// </summary>
         public readonly string Name;
+
+        /// <summary>
+        /// The profession of the character
+        /// </summary>
         public readonly int ProfessionIndex;
+
+        /// <summary>
+        /// The race of the character
+        /// </summary>
         public readonly int RaceIndex;
+
+        /// <summary>
+        /// The race the character was born with (they might have been polymorphed or mutated)
+        /// </summary>
         public readonly int RaceIndexAtBirth;
+
+        /// <summary>
+        /// The character's first realm of magic (if any)
+        /// </summary>
         public readonly Realm Realm1;
+
+        /// <summary>
+        /// The character's second realm of magic (if any)
+        /// </summary>
         public readonly Realm Realm2;
 
+        /// <summary>
+        /// Make an ex-player from a player, remembering the essential information about the character
+        /// </summary>
+        /// <param name="player"> The player character from which to create the ex player </param>
         public ExPlayer(Player player)
         {
             GenderIndex = player.GenderIndex;
